0a0818b79663fef7056ab52da7f949b8b0413f20
BiggBaddw00f <tunjebrown@gmail.com>
2023-07-04 13:13:06 +0300

    Adding the response to the request

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = express()
 
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+app.listen(port, () => console.log(`App is listening on port ${port}!`))
