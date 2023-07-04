4f51eadfc299b239d14493365165d5f7c25d1cfe
BiggBaddw00f <tunjebrown@gmail.com>
2023-07-04 14:24:32 +0300

    Adding the calculateBMI function

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const bodyParser = require('body-parser');
 const express = require('express')
 const path = require('path');
 const app = express()
+const port = 5500
 
-const port = 3000
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+app.post('/', (req, res) => {
+
+    var height = parseInt(req.body.height);
+    var weight = parseInt(req.body.weight);
+    const calculateBMI = () => {
+        var heightInMeters = height / 100;
+        var bmi = weight / (heightInMeters * heightInMeters);
+        return bmi.toFixed(2);
+    };
+    var bmiResult = calculateBMI();
+
+    res.send("Your BMI is: " + bmiResult);
+})
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
